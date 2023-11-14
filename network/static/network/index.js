d17e4070f184998bc6aef906f5c8a229372917e8
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2023-11-14 14:00:19 +0000

    unfollow button setup

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -1,1 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+  document
+    .querySelector('#follow-btn')
+    .addEventListener('click', () => load_follow());
+});
 
+function load_follow() {
+  const btnForm = document.querySelector('#following-status');
+
+  const newBtn = document.createElement('button');
+  newBtn.classList.add('btn', 'btn-outline-danger', 'unfollow-btn');
+  newBtn.type = 'submit';
+  newBtn.innerHTML = 'Unfollow';
+  btnForm.appendChild(newBtn);
+}
